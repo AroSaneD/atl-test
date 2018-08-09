3cbd32522fc6dacbd6f924e861bad7b64c28f030
AroSaneD <mail.junk.3135@gmail.com>
2018-08-10 01:23:40 +0300

    implemented game thumbnails with routing

--- src/app/components/game-list/game-list.component.ts
+++ src/app/components/game-list/game-list.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Select } from '@ngxs/store';
+
+import { Observable } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+
+import { GameModel } from '../../model/gameModel';
+import { ModelState } from '../../model/model.state';
+import { Game } from '../../model/game';
+
+class GameThumbnail {
+    urlName: string;
+    link: string;
+
+    constructor(public name: string, public image: string) {
+        this.urlName = name.replace(' ', '-').toLowerCase();
+        this.link = `/${this.urlName}/search`;
+    }
+}
 
 @Component({
-  selector: 'app-game-list',
-  templateUrl: './game-list.component.html',
-  styleUrls: ['./game-list.component.css']
+    selector: 'app-game-list',
+    templateUrl: './game-list.component.html',
+    styleUrls: ['./game-list.component.css']
 })
 export class GameListComponent implements OnInit {
+    @Select((state: { gameModel: GameModel }) => {
+        return Array.from(state.gameModel.games).map(
+            ([name, game]) => new GameThumbnail(name, game.image_url)
+        );
+    })
+    games: Observable<GameThumbnail[]>;
 
-  constructor() { }
+    constructor() {
+        this.games.subscribe(data => console.log('Games: ', data));
+    }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
